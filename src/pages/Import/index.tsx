3f8b342c19de023f502db86c378737d798b704a3
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2021-01-18 21:54:21 -0300

    Adiciona rotas de páginas e comportamento de Import

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    const firstFile = uploadedFiles[0];
+
+    data.append('file', firstFile.file, firstFile.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadFileList = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadFileList);
   }
 
   return (
